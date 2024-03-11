47c4886bc1f8800325842651543905961e32e8af
piercenovo <piercenovoah@gmail.com>
2024-03-11 12:19:37 -0500

    build: update packages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true
-  },
   images: {
-    domains: ['res.cloudinary.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com'
+      }
+    ]
   }
 }
 
